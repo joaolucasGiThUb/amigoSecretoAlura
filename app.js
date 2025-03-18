25d8940c5c6f35b24d0e7f338d21bb89b2049c2a
João Lucas B. Araujo <61123581+joaolucasGiThUb@users.noreply.github.com>
2025-03-18 16:59:56 -0300

    upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let nome = "";
+
+//salvar nome
+function salvarNome() {
+   nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert("Por favor, insira seu nome");
+    } else {
+        listaDeAmigos.push(nome);
+        document.getElementById("nome").value = "";
+        atualizarLista();
+    }
+}
+
+let listaDeAmigos = [];
+
+//adicionar amigo na lista
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome");
+    } else {
+        listaDeAmigos.push(nomeAmigo);
+        document.getElementById("amigo").value ="";
+        atualizarLista();
+    }
+}
+//atualizar a lista de amigos
+function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
+    listaAtualizada.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaDeAmigos[i];
+        listaAtualizada.appendChild(itemLista);
+    }
+}
+//sortear amigo 
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("Por favor, insira um nome.");
+        return;
+
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let nomeSorteado = listaDeAmigos[indiceAleatorio];
+    if (nomeSorteado == nome) {
+        alert("Você não pode ser seu próprio amigo secreto! Por favor, tente novamente.");
+        return;
+    }
+
+    document.getElementById("resultado").innerHTML = `Seu amigo secreto é: ${nomeSorteado}`;
+    
+}  
